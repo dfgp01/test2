ca13cfe03e53375d9f2e3f83d388619fb1a1332a
dfgp01 <250266148@qq.com>
2015-06-30 18:06:01 +0800

    就改了一点点。

--- src/system/ActionSystem.js
+++ src/system/ActionSystem.js
@@ -20,3 +20,18 @@ IdleActionSystem = ActionSystem.extend({
 		//未完
 	}
 });
+
+WalkActionSystem = ActionSystem.extend({
+	start : function(unit, dt){
+		if(unit.motionCom.dx < 1){
+			unit.viewCom.sprite._scaleX = -1;
+		}else{
+			unit.viewCom.sprite._scaleX = 1;
+		}
+	},
+	
+	update : function(unit, dt){
+		unit.viewCom.sprite.x += unit.motionCom.dx;
+		unit.viewCom.sprite.y += unit.motionCom.dy;
+	}
+});
